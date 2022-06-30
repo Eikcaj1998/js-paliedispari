f8d9eec88dfc46d1f1e60dcd9e85729b0237b0f5
Eikcaj.1998 <jw220998@gmail.com>
2022-06-30 09:35:36 +0200

    validazione

--- pari-e-dispari/js/script.js
+++ pari-e-dispari/js/script.js
@@ -11,42 +11,58 @@ Dichiariamo chi ha vinto. */
 // ?3 - Devo restituire qualcosa? (se si cosa? di che tipo?)
 // ?4 OPZIONALE:  Qualcosa  potrebbe andare storto? (se si, cosa?) */
 
+//scelgiamo tra pari o dispari
+const userChoice = prompt('scegli "pari" o "dispari"','pari');
+console.log('Hai scelto' + ' ' + userChoice);
 
-//numeri random usando una funzione
-function randomNumber(number1, number2){
-    return Math.floor(number1 + number2 * Math.random());
- }
- 
- //scelgiamo tra pari o dispari
- const userChoice = prompt('scegli pari o dispari','pari');
- console.log('Hai scelto' + ' ' + userChoice);
- //scegliamo un numero tra l' uno ed il 5
- const userNumberChioce = parseInt(prompt('scegli un numero tra l\'uno ed il 5','5'));
- console.log('Hai scelto il numero' + ' ' + userNumberChioce);
- // il pc sceglie un numero
- const pcNumber = randomNumber(1, 5);
- console.log('il computer ha fatto' + ' ' + pcNumber);
+//scegliamo un numero tra l' uno ed il 5
+const userNumber = parseInt(prompt('scegli un numero tra l\'uno ed il 5','5'));
+console.log('Hai scelto il numero' + ' ' + userNumber);
 
- let sum = pcNumber + userNumberChioce;
- 
- function isEven(totalNumber){
-     let result = 'Pari';
-     if (totalNumber % 2 !== 0){
-         return result = 'Dispari';
-        }
-        return result
-    }
-
-
-const checkResult = isEven(sum);
-console.log('il numero sommato e' + ' ' + sum + ' ' + checkResult);
-
- if (((userChoice === 'pari') && (sum % 2 == 0)) || ((userChoice === 'dispari') && (sum % 2 !== 0))) {
-     console.log('Hai Vinto');
-
+//validazione
+let isValid = true;
+let message = '';
+if( userChoice !== 'pari' && userChoice !== 'dispari'){
+    isValid = false;
+    message = "devi scegliere pari o dispari";
+}
+if (isNaN(userNumber)|| userNumber < 1 || userNumber > 5){
+    isValid = false;
+    message += " il numero scelto non e valido";
+}
+if (isValid){
     
- } else {
-     console.log('Hai Perso');
- }
- 
+    //numeri random usando una funzione
+    function randomNumber(number1, number2){
+        return Math.floor(number1 + number2 * Math.random());
+     }
+     
+     // il pc sceglie un numero
+     const pcNumber = randomNumber(1, 5);
+     console.log('il computer ha fatto' + ' ' + pcNumber);
+     let sum = pcNumber + userNumber;
+     
+     function isEven(totalNumber){
+         let result = 'Pari';
+         if (totalNumber % 2 !== 0){
+             return result = 'Dispari';
+            }
+            return result
+        }
+    
+    const checkResult = isEven(sum);
+    console.log('il numero sommato e' + ' ' + sum + ' ' + checkResult);
+    
+    if (((userChoice === 'pari') && (sum % 2 == 0)) || ((userChoice === 'dispari') && (sum % 2 !== 0))) {
+        alert('Hai Vinto');
+
+        
+        
+    } else {
+        alert('Hai Perso');
+    }
+    
+}else{
+    alert(message)
+}
  
